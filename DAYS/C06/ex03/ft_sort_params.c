e26dfc923c32b685da887f6cd83a63145c393e19
Cl9U <farajisalah496@gmail.com>
2026-07-14 14:19:47 +0100

    C05/C06

---
+++ DAYS/C06/ex03/ft_sort_params.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		j;
+	char	*swp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+			{
+				swp = av[i];
+				av[i] = av[j];
+				av[j] = swp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
